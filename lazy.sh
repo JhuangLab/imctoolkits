c626eef8ef80f5ca41e526579002862b42f095d9
Jinyan <jinyancool@163.com>
2022-08-24 10:58:06 +0800

    del lazy.sh

--- lazy.sh
+++
@@ -1,7 +0,0 @@
-alias bfg="java -jar ~/bin/tools/bfg-1.14.0.jar"
-git add .
-git commit -m "Lazy commit"
-git commit --amend -CHEAD
-bfg --strip-blobs-bigger-than 50M
-git reflog expire --expire=now --all && git gc --prune=now --aggressive
-git push
